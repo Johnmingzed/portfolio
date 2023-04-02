defa3099bf4f35a5aad4358e39b7922524535099
Jonathan PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-04-02 16:58:03 +0200

    ajout de fichiers

--- test.php
+++ test.php
@@ -3,3 +3,6 @@
 // Script PHP de nettoyage des fichiers desktop.ini côté serveur
 echo 'TEST';
 echo 'TEST 2';
+Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Rerum, harum asperiores architecto quis, aperiam, accusantium velit dolores 
+laborum illum fugit expedita minus quaerat officiis maiores consequuntur fuga nihil unde magni!
--- test2.php
+++ test2.php
@@ -1,1 +1,4 @@
 blabla
+blabla
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta nesciunt repudiandae laborum aut harum consequuntur
+cumque quia aspernatur quisquam eligendi molestias molestiae, ea adipisci dolores illum magnam quis, quam dignissimo
