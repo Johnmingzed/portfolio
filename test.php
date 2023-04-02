5a086867e4341f7ee4f4111b222659ae32ccfe09
Jonathan PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-04-02 17:04:43 +0200

    ecrasement ?

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Script PHP de nettoyage des fichiers desktop.ini côté serveur
-echo 'TEST';
-echo 'TEST 2';
-Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Rerum, harum asperiores architecto quis, aperiam, accusantium velit dolores 
-laborum illum fugit expedita minus quaerat officiis maiores consequuntur fuga nihil unde magni!
--- test2.php
+++
@@ -1,4 +0,0 @@
-blabla
-blabla
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta nesciunt repudiandae laborum aut harum consequuntur
-cumque quia aspernatur quisquam eligendi molestias molestiae, ea adipisci dolores illum magnam quis, quam dignissimo
